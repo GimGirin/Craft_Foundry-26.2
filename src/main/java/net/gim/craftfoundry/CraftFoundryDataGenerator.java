2fe3a9c72066be617a6a62db3a339d36ce915be5
Theodore Gim <theodoreyjg@gmail.com>
2026-09-11 02:38:50 -0700

    1

---
+++ src/main/java/net/gim/craftfoundry/CraftFoundryDataGenerator.java
@@ -0,0 +1,11 @@
+package net.gim.craftfoundry;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CraftFoundryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
